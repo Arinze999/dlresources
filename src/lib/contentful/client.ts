de65a81d284c76b7fac7d50173ac11e78bd45150
Arinze999 <akanagapyt@gmail.com>
2026-03-10 00:43:38 +0100

    fizzzz3

--- src/lib/contentful/client.ts
+++ src/lib/contentful/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'contentful';
 
-const space = process.env.CONTENTFUL_SPACE_ID;
-const accessToken = process.env.CONTENTFUL_DELIVERY_TOKEN;
-const environment = process.env.CONTENTFUL_ENVIRONMENT || 'master';
+const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_DELIVERY_TOKEN;
+const environment = process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT || 'master';
 
 if (!space) {
   throw new Error('Missing CONTENTFUL_SPACE_ID');
